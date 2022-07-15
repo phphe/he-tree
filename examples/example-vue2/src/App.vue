6bc2abc11564b34f56f7464601e8a532b6492573
phphe <phphe@outlook.com>
2022-07-15 17:57:57 +0800

    docs(examples): add vue2 example

---
+++ examples/example-vue2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Draggable v-model="data" ref="tree" virtualization style="height: 500px">
+      <template #default="{ node, stat }">
+        <span v-if="stat.children.length" @click="stat.open = !stat.open">
+          {{ stat.open ? "-" : "+" }}
+        </span>
+        <!-- <span v-else>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <input type="checkbox" v-model="stat.checked" /> -->
+        {{ node.text }}
+      </template>
+    </Draggable>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Draggable } from "@he-tree/vue/vue2";
+import data from "./data.json";
+
+@Component({
+  components: { Draggable },
+})
+export default class App extends Vue {
+  data = data;
+}
+</script>
+
+<style></style>
